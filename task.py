833db496330eeb86b3f6e53ccea6fd4e1f7c6c6b
dliangsta <davidliangx27@gmail.com>
2017-04-24 22:57:49 -0500

    for resize, invert

---
+++ task.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import os, sys
+
+path = "sketch/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((64,64), Image.ANTIALIAS)
+            imResize.save(f + '.png', 'PNG', quality=100)
+
+def invert():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            inverted_image = PIL.ImageOps.invert(im)
+            inverted_image.save(f + '.png', 'PNG', quality=100)
+
+#resize()
+
+#invert()
