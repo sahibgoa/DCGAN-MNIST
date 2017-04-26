285aea9639c905a7b13fb6cba17b1f68f82d9605
dliangsta <davidliangx27@gmail.com>
2017-04-25 22:28:52 -0500

    extra layer

--- discriminator.py
+++ discriminator.py
@@ -15,24 +15,27 @@ class Discriminator(object):
         self.x = tf.concat([self.x_real, self.x_fake], axis=0)
 
         # weights of each layer
-        self.w1 = tf_gaussian([DIM_IM, DIM_H2])
-        self.w2 = tf_gaussian([DIM_H2, DIM_H1])
-        self.w3 = tf_gaussian([DIM_H1, 1])
+        self.w1 = tf_gaussian([DIM_IM, DIM_H3])
+        self.w2 = tf_gaussian([DIM_H3, DIM_H2])
+        self.w3 = tf_gaussian([DIM_H2, DIM_H1])
+        self.w4 = tf_gaussian([DIM_H1, 1])
 
         # weights of biases
-        self.b1 = tf_zeros([DIM_H2])
-        self.b2 = tf_zeros([DIM_H1])
-        self.b3 = tf_zeros([1])
+        self.b1 = tf_zeros([DIM_H3])
+        self.b2 = tf_zeros([DIM_H2])
+        self.b3 = tf_zeros([DIM_H1])
+        self.b4 = tf_zeros([1])
 
         # layers
         self.h1 = tf.nn.dropout(tf_relu(self.x, self.w1, self.b1), KEEP_PROB)
         self.h2 = tf.nn.dropout(tf_relu(self.h1, self.w2, self.b2), KEEP_PROB)
-        self.h3 = tf.matmul(self.h2, self.w3) + self.b3
+        self.h3 = tf.nn.dropout(tf_relu(self.h2, self.w3, self.b3), KEEP_PROB)
+        self.h4 = tf.matmul(self.h3, self.w4) + self.b4
 
         # separate discrimination of real and fake data
-        self.y_real = tf.nn.sigmoid(tf.slice(self.h3, [0, 0], [BATCH_SIZE, -1]))
-        self.y_fake = tf.nn.sigmoid(tf.slice(self.h3, [BATCH_SIZE, 0], [-1, -1]))
+        self.y_real = tf.nn.sigmoid(tf.slice(self.h4, [0, 0], [BATCH_SIZE, -1]))
+        self.y_fake = tf.nn.sigmoid(tf.slice(self.h4, [BATCH_SIZE, 0], [-1, -1]))
 
         # trainable parameters
-        self.params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3]
+        self.params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3, self.w4, self.b4]
 
--- generator.py
+++ generator.py
@@ -13,20 +13,23 @@ class Generator(object):
         # weights of each layer
         self.w1 = tf_gaussian([DIM_Z, DIM_H1])
         self.w2 = tf_gaussian([DIM_H1, DIM_H2])
-        self.w3 = tf_gaussian([DIM_H2, DIM_IM])
+        self.w3 = tf_gaussian([DIM_H2, DIM_H3])
+        self.w4 = tf_gaussian([DIM_H3, DIM_IM])
 
         # weights of biases
         self.b1 = tf_zeros([DIM_H1])
         self.b2 = tf_zeros([DIM_H2])
-        self.b3 = tf_zeros([DIM_IM])
+        self.b3 = tf_zeros([DIM_H3])
+        self.b4 = tf_zeros([DIM_IM])
 
         # layers
         self.h1 = tf_relu(self.z, self.w1, self.b1)
         self.h2 = tf_relu(self.h1, self.w2, self.b2)
-        self.h3 = tf.matmul(self.h2, self.w3) + self.b3
+        self.h3 = tf_relu(self.h2, self.w3, self.b3)
+        self.h4 = tf.matmul(self.h3, self.w4) + self.b4
 
         # output
-        self.x_fake = tf.nn.tanh(self.h3)
+        self.x_fake = tf.nn.tanh(self.h4)
 
         # trainable parameters
-        self.params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3]
+        self.params = [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3, self.w4, self.b4]
